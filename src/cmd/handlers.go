bcf0cf658e808eca516bd88bde6d9344a64197df
wotmshuaisi <charlanders1@gmail.com>
2019-02-25 03:58:10 +0800

    audio handler demo handler

--- src/cmd/handlers.go
+++ src/cmd/handlers.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-	"time"
+	"github.com/google/uuid"
 
 	tba "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/sirupsen/logrus"
 )
 
-func testhandler(ctx *tba.Update) {
-	// max
+func testhandler(bot *tba.BotAPI, ctx *tba.Update) {
+	// task limiter
 	inlineQueryWG.Add(1)
 	inlineTasksCount++
 	defer func() {
 		inlineTasksCount--
 		inlineQueryWG.Done()
 	}()
+	// processing event
+	u, _ := uuid.NewUUID()
+	i := tba.NewInlineQueryResultAudio(u.String(), "http://fs.open.kugou.com/05914e4cdceb1d0e406dac5a0722168c/5c72eabb/G140/M04/00/01/LIcBAFvMQmaAVJNYAEgvmkjJyY0089.mp3", "Hello - Adele")
 
-	logrus.Infof("offset: %s content: %s", ctx.InlineQuery.Offset, ctx.InlineQuery.Query)
-	time.Sleep(time.Second * 40)
+	config := tba.InlineConfig{
+		InlineQueryID: ctx.InlineQuery.ID,
+		IsPersonal:    true,
+		Results:       []interface{}{&i},
+	}
+
+	res, err := bot.AnswerInlineQuery(config)
+	if err != nil || !res.Ok {
+		logrus.WithError(err).Warnf("content: %s res: %+v", ctx.InlineQuery.Query, res)
+	}
+
 }
